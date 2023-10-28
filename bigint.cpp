9e2f027219b3c1707b8c16e9b93089014dfaf5d9
DukeOfBears <alexei_beliz@bk.ru>
2023-10-28 19:28:42 +0300

    remove 'length', add operator *= prototype

--- bigint.cpp
+++ bigint.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <vector>
-
+// binary division
 class BigInt {
 private:
   static const int base = 10;
-  size_t length = 0;
   std::vector<int> digits; 
   bool positive = true;
   void converse() {
-    for (size_t i = 0; i < length/2; ++i) {
+    for (size_t i = 0; i < digits.size()/2; ++i) {
       std::swap(digits[i], digits[digits.size() - 1 - i]);
     }
   }
 
   void resize(const BigInt& right) {
-    if (length < right.length) {
-      length = right.length;
-      digits.resize(length);
+    if (digits.size() < right.digits.size()) {
+      digits.resize(digits.size());
     }
   }
 
 public:
   BigInt() = default;
 
-  BigInt(const std::string& str): length(str.size()), digits(length) {
+  BigInt(const std::string& str): digits(str.size()) {
     if (str.front() == '-') {
       positive = false;
-      digits.resize(--length);
+      digits.resize(digits.size() - 1);
     }
-    for (size_t i = 0; i < length; ++i) {
+    for (size_t i = 0; i < digits.size(); ++i) {
       digits[i] = std::stoi(str.substr(str.size() - 1 - i, 1));
     }
   }
 
-  BigInt(int64_t num) {
+  explicit BigInt(int64_t num) {
     if (num < 0) {
       positive = false;
       num *= -1;
     }
     while (num > 0) {
       digits.push_back(num % base);
-      ++length;
       num /= base;  
     }
   }
@@ -49,7 +46,7 @@ public:
     if (positive && right.positive) {
       resize(right); 
       size_t excess = 0;
-      for (size_t i = 0; i < right.length; ++i) {
+      for (size_t i = 0; i < right.digits.size(); ++i) {
         (digits[i] += right.digits[i]) += excess;
         excess = 0;
         if (digits[i] >= base) {
@@ -59,7 +56,6 @@ public:
       }
       if (excess) {
         digits.push_back(excess);
-        ++length;
       }
       return *this;
     } 
@@ -69,42 +65,69 @@ public:
   BigInt& operator-=(const BigInt& right) {
     resize(right);
     size_t less = 0;
-      for (size_t i = 0; i < right.length; ++i) {
+      for (size_t i = 0; i < right.digits.size(); ++i) {
         if (right.positive) {
           (digits[i] -= right.digits[i]) -= less; 
         } else {
-          (digits[i] += right.digits[i]) -= less;
+          
         }
         less = 0;
         if (digits[i] < 0) {
           less += 1;
           digits[i] += base;
         }
+        
       }
       if (less) {
-        positive = !positive;
-        digits.front() -= base;
-        digits.front() *= -1;
-        for (size_t i = 1; i < length; ++i) {
-          digits[i] -= base - 1;
-          digits[i] *= -1;
+        if (!positive && right.positive) {
+          digits.push_back(less);
+        } else {
+          positive = !positive;
+          digits.front() -= base;
+          digits.front() *= -1;
+          for (size_t i = 1; i < digits.size(); ++i) {
+            digits[i] -= base - 1;
+            digits[i] *= -1;
+          }
         }
       }
       return *this;
   }
+  BigInt& operator*=(const BigInt& right) {
+    std::vector<int> res(digits.size() + right.digits.size() + 1);
+    for (size_t i = 0; i < digits.size(); ++i) {
+      for (size_t j = 0; j < right.digits.size(); ++j) {
+        res[i + j] += digits[i] * right.digits[j];
+        if (res[i + j] >= base) {
+          res[i + j + 1] += res[i + j] / base;
+          res[i + j] %= base;
+        }
+      }
+    }
+    while (res.back() == 0 && res.size() != 1) res.pop_back();
+    digits = std::move(res);
+    return *this;
+  }
+
+  BigInt operator*(const BigInt& right) const {
+    BigInt copy = *this;
+    copy *= right;
+    return copy;
+  }
+
 
   bool operator<(const BigInt& right) const {
     if (!positive && right.positive) return true;
-    if (positive && right.positive && (length < right.length)) return true;
-    if (positive && right.positive && length == right.length) {
-      for (size_t i = 0; i < length; ++i) {
-        if (digits[length - i - 1] < right.digits[length - i - 1]) return true;  
+    if (positive && right.positive && (digits.size() < right.digits.size())) return true;
+    if (positive && right.positive && digits.size() == right.digits.size()) {
+      for (size_t i = 0; i < digits.size(); ++i) {
+        if (digits[digits.size() - i - 1] < right.digits[digits.size() - i - 1]) return true;  
       }
     }
-    if (!positive && !right.positive && (right.length < length)) return true;
-    if (!positive && !right.positive && (right.length == length)) {
-      for (size_t i = 0; i < length; ++i) {
-        if (right.digits[length - i - 1] < digits[length - i - 1]) return true;
+    if (!positive && !right.positive && (right.digits.size() < digits.size())) return true;
+    if (!positive && !right.positive && (right.digits.size() == digits.size())) {
+      for (size_t i = 0; i < digits.size(); ++i) {
+        if (right.digits[digits.size() - i - 1] < digits[digits.size() - i - 1]) return true;
       }
     }
     return false;
@@ -115,8 +138,8 @@ public:
   }
 
   bool operator==(const BigInt& right) const {
-    if (length == right.length && positive == right.positive) {
-      for (size_t i = 0; i < length; ++i) {
+    if (digits.size() == right.digits.size() && positive == right.positive) {
+      for (size_t i = 0; i < digits.size(); ++i) {
         if (digits[i] != right.digits[i]) return false;
       }
       return true;
@@ -126,7 +149,7 @@ public:
 
 
   size_t size() const {
-    return length;
+    return digits.size();
   }
 
   bool is_positive() const {
@@ -165,11 +188,13 @@ int main() {
   std::cout << c.size() << '\n';
   std::cout << c << '\n';
   */
-  std::string s1 = "186";
-  std::string s2 = "3215";
+  std::string s1 = "128";
+  std::string s2 = "256";
   BigInt a = s1;
   BigInt b = s2;
-  std::cout << (a -= b) << '\n';
+  std::cout << a << ' ' << b << '\n';
+  std::cout << (a *= b) << '\n';
+  
   
 
 }
